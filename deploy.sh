af2bfb8c9494f892220d866cbf10f3c3e4fbfe38
Melkor-1 <harith.bin.salam@gmail.com>
2026-01-20 07:14:59 -1200

    Fix-script: Do not ask for approval to install apache2.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install apache2
+sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo systemctl status apache2
